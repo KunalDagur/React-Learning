9e7405454c21ae88af6b53d3e2f130b1bb66ce89
KunalDagur <kdagur34@gmail.com>
2024-09-04 18:12:40 +0530

    Start of React and Value Updation HOOKS use

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <h1>chai aur code | Kunal</h1>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 0
+
+  const addValue = () => {
+    counter++
+    if (counter <= 20) {
+      setCounter(counter)
+      console.log("Value : " + counter)
+    } else {
+      alert("Counter can't go above 20")
+    }
+  }
+  const decreaseValue = () => {
+    counter--
+    if (counter >= 0) {
+      setCounter(counter)
+      console.log("Value : " + counter)
+    } else {
+      alert("Counter can't go below 0")
+
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue} >Add Value</button>
+      <button onClick={decreaseValue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
